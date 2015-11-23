f6c4639613f25b2b9bf3a543213024153ee280cd
dhealf <dhealf@gmail.com>
2015-11-23 17:39:33 +0700

    multiple process

--- application/views/antenna/upload.php
+++ application/views/antenna/upload.php
@@ -44,7 +44,7 @@
                         <div class="col-md-12 col-xs-12">
                             <div class="x_panel">
                                 <div class="x_title">
-                                    <h2>Form Basic Elements <small>different form elements</small></h2>
+                                    <h2>Antenna<small>Upload Data</small></h2>
                                     <ul class="nav navbar-right panel_toolbox">
                                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                         </li>
