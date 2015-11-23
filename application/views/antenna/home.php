61e10b5a10f81bbfc3735c444a609118f6bc7ec4
dhealf <dhealf@gmail.com>
2015-11-23 17:33:49 +0700

    multiple process

--- application/views/antenna/home.php
+++ application/views/antenna/home.php
@@ -28,27 +28,6 @@
             <!-- page content -->
             <div class="right_col" role="main">
                 <div class="">
-                    <div class="page-title">
-                        <div class="title_left">
-                            <h3>
-                                Invoice
-                                <small>
-                                    Some examples to get you started
-                                </small>
-                            </h3>
-                        </div>
-
-                        <div class="title_right">
-                            <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search for...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default" type="button">Go!</button>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="clearfix"></div>
 
                     <div class="row">
@@ -56,7 +35,7 @@
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <div class="x_panel">
                                 <div class="x_title">
-                                    <h2>Daily active users <small>Sessions</small></h2>
+                                    <h2>Antenna <small>All Antenna Data</small></h2>
                                     <ul class="nav navbar-right panel_toolbox">
                                         <li><a href="#"><i class="fa fa-chevron-up"></i></a>
                                         </li>
