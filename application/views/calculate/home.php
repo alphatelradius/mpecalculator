57bb1d79d9caf58fba8c2c2f9626053739b7d696
dhealf <dhealf@gmail.com>
2015-11-23 17:50:26 +0700

    multiple process

--- application/views/calculate/home.php
+++ application/views/calculate/home.php
@@ -36,7 +36,7 @@
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <div class="x_panel">
                                 <div class="x_title">
-                                    <h2>Daily active users <small>Sessions</small></h2>
+                                    <h2>MPE <small>All done process</small></h2>
                                     <ul class="nav navbar-right panel_toolbox">
                                         <li><a href="#"><i class="fa fa-chevron-up"></i></a>
                                         </li>
